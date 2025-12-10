129969972cace7268437064247d7736adf806a8a
ErlingSigurdson <efimov-d-v@yandex.ru>
2025-12-11 00:26:17 +0300

    Divergence started (working version).

--- src/Drv7SegQ595.h
+++ src/Drv7SegQ595.h
@@ -1,10 +1,10 @@
 /*************** FILE DESCRIPTION ***************/
 
 /**
- * Filename: Drv7Seg2x595.h
+ * Filename: Drv7SegQ595.h
  * ----------------------------------------------------------------------------|---------------------------------------|
  * Purpose:  A class for driving a multiplexed 7-segment display using
- *           two daisy-chained 74HC595 shift register ICs.
+ *           a single 74HC595 shift register IC and 1 to 4 transistors.
  * ----------------------------------------------------------------------------|---------------------------------------|
  * Notes:    Refer to the README for a general library overview and
  *           a basic API usage description.
@@ -12,17 +12,14 @@
  *           Intended for displays with 1 to 4 character positions (digits).
  *
  *           seg_byte means a byte that turns ON and OFF individual segments.
- *
- *           pos_byte means a byte that turns ON and OFF whole character
- *           positions (digits).
  */
 
 
 /************ PREPROCESSOR DIRECTIVES ***********/
 
 // Include guards.
-#ifndef DRV7SEG2X595_H
-#define DRV7SEG2X595_H
+#ifndef DRV7SEGQ595_H
+#define DRV7SEGQ595_H
 
 
 /*--- Includes ---*/
@@ -53,26 +50,26 @@
 #define DRV7SEG2X595_LSB                   0
 #define DRV7SEG2X595_ONLY_LSB_SET_MASK     0x01u
 #define DRV7SEG2X595_ONLY_MSB_SET_MASK     (DRV7SEG2X595_ONLY_LSB_SET_MASK << DRV7SEG2X595_MSB)
-#define DRV7SEG2X595_ALL_BITS_SET_MASK     0xFF
 #define DRV7SEG2X595_ALL_BITS_CLEARED_MASK 0x00
+
+#define DRV7SEG2X595_POS_PIN_INITIAL -1
 
 // Driver configuration status codes. Double as return codes for some methods.
 #define DRV7SEG2X595_STATUS_INITIAL                     -1
 #define DRV7SEG2X595_STATUS_ERR_VARIANT_NOT_SPECIFIED   -2
-#define DRV7SEG2X595_STATUS_ERR_INVALID_BYTE_ORDER      -3
-#define DRV7SEG2X595_STATUS_ERR_INVALID_POS_SWITCH_TYPE -4
-#define DRV7SEG2X595_STATUS_ERR_INVALID_POS_BIT         -5
-#define DRV7SEG2X595_STATUS_ERR_POS_BIT_DUPLICATION     -6
+#define DRV7SEG2X595_STATUS_ERR_INVALID_POS_SWITCH_TYPE -3
+#define DRV7SEG2X595_STATUS_ERR_INVALID_POS_PIN         -4
+#define DRV7SEG2X595_STATUS_ERR_POS_PIN_DUPLICATION     -5
 #define DRV7SEG2X595_STATUS_OK                           0
 
 // set_glyph_to_pos() method additional return codes.
-#define DRV7SEG2X595_SET_GLYPH_ERR_INVALID_POS                   -7
-#define DRV7SEG2X595_SET_GLYPH_ERR_POS_BIT_NOT_SPECIFIED_FOR_POS -8
+#define DRV7SEG2X595_SET_GLYPH_ERR_INVALID_POS                   -6
+#define DRV7SEG2X595_SET_GLYPH_ERR_POS_PIN_NOT_SPECIFIED_FOR_POS -7
 #define DRV7SEG2X595_SET_GLYPH_OK                                 0
 
 // output() method additional return codes.
-#define DRV7SEG2X595_OUTPUT_ERR_INVALID_POS                   -7
-#define DRV7SEG2X595_OUTPUT_ERR_POS_BIT_NOT_SPECIFIED_FOR_POS -8
+#define DRV7SEG2X595_OUTPUT_ERR_INVALID_POS                   -6
+#define DRV7SEG2X595_OUTPUT_ERR_POS_PIN_NOT_SPECIFIED_FOR_POS -7
 #define DRV7SEG2X595_OUTPUT_NEXT                               0
 #define DRV7SEG2X595_OUTPUT_ANTI_GHOSTING_RETENTION_RUNNING    1
 
@@ -105,30 +102,13 @@
 
 /****************** DATA TYPES ******************/
 
-class Drv7Seg2x595Class {
+class Drv7SegQ595Class {
     public:
         /*--- Data types ---*/
-
-        enum class ByteOrder {
-            PosByteFirst = 0,
-            SegByteFirst = 1
-        };
 
         enum class PosSwitchType {
             ActiveLow  = 0,
             ActiveHigh = 1
-        };
-
-        enum class PosBit {
-            PosBitInitial = -1,
-            PosBit0       =  DRV7SEG2X595_LSB,  // 0
-            PosBit1       =  1,
-            PosBit2       =  2,
-            PosBit3       =  3,
-            PosBit4       =  4,
-            PosBit5       =  5,
-            PosBit6       =  6,
-            PosBit7       =  DRV7SEG2X595_MSB   // 7
         };
 
         enum class Pos {
@@ -142,7 +122,7 @@ class Drv7Seg2x595Class {
         /*--- Methods ---*/
 
         // Default constructor.
-        Drv7Seg2x595Class();
+        Drv7SegQ595Class();
 
         /* Configure the driver to use bit-banging.
          *
@@ -150,27 +130,23 @@ class Drv7Seg2x595Class {
          * a negative integer otherwise (see the preprocessor macros list for possible values).
          *
          * Parameters:
-         * - byte_order                     - within a 16-bit register formed by two ICs either
-         *                                    pos_byte is an upper byte and seg_byte is a lower byte or
-         *                                    seg_byte is an upper byte and pos_byte is a lower byte.
          * - pos_switch_type                - character positions are turned on either by
          *                                    set (active-high) or cleared (active-low) pos_byte bits.
          * - data_pin, latch_pin, clock_pin - pins used for bit-banging and latching.
-         * - pos_N_bit                      - pos_byte bits that control character positions.
+         * - pos_N_pin                      - pos_byte bits that control character positions.
          *                                    pos_1_bit is required, other bits are optional
          *                                    (respective parameters can be omitted).
          *
          * Multiple calls to this method are valid, each call leads to a fresh configuration.
          */
-        int32_t begin_bb(ByteOrder byte_order,
-                         PosSwitchType pos_switch_type,
+        int32_t begin_bb(PosSwitchType pos_switch_type,
                          uint32_t data_pin,
                          uint32_t latch_pin,
                          uint32_t clock_pin,
-                         PosBit pos_1_bit,
-                         PosBit pos_2_bit = PosBit::PosBitInitial,
-                         PosBit pos_3_bit = PosBit::PosBitInitial,
-                         PosBit pos_4_bit = PosBit::PosBitInitial
+                         int32_t pos_1_pin,
+                         int32_t pos_2_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                         int32_t pos_3_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                         int32_t pos_4_pin = DRV7SEG2X595_POS_PIN_INITIAL
                         );
 
         /* Configure the driver to use SPI with default pins.
@@ -187,13 +163,12 @@ class Drv7Seg2x595Class {
          * Safety of multiple calls to this method depends on the SPI.h implementation.
          */
         #ifdef DRV7SEG2X595_SPI_PROVIDED
-        int32_t begin_spi(ByteOrder byte_order,
-                          PosSwitchType pos_switch_type,
+        int32_t begin_spi(PosSwitchType pos_switch_type,
                           uint32_t latch_pin,
-                          PosBit pos_1_bit,
-                          PosBit pos_2_bit = PosBit::PosBitInitial,
-                          PosBit pos_3_bit = PosBit::PosBitInitial,
-                          PosBit pos_4_bit = PosBit::PosBitInitial
+                          int32_t pos_1_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                          int32_t pos_2_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                          int32_t pos_3_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                          int32_t pos_4_pin = DRV7SEG2X595_POS_PIN_INITIAL
                          );
         #endif
 
@@ -211,15 +186,14 @@ class Drv7Seg2x595Class {
          * Safety of multiple calls to this method depends on the SPI.h implementation.
          */
         #ifdef DRV7SEG2X595_SPI_PROVIDED_CUSTOM_PINS
-        int32_t begin_spi_custom_pins(ByteOrder byte_order,
-                                      PosSwitchType pos_switch_type,
+        int32_t begin_spi_custom_pins(PosSwitchType pos_switch_type,
                                       uint32_t mosi_pin,
                                       uint32_t latch_pin,
                                       uint32_t sck_pin,
-                                      PosBit pos_1_bit,
-                                      PosBit pos_2_bit = PosBit::PosBitInitial,
-                                      PosBit pos_3_bit = PosBit::PosBitInitial,
-                                      PosBit pos_4_bit = PosBit::PosBitInitial
+                                      int32_t pos_1_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                                      int32_t pos_2_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                                      int32_t pos_3_pin = DRV7SEG2X595_POS_PIN_INITIAL,
+                                      int32_t pos_4_pin = DRV7SEG2X595_POS_PIN_INITIAL
                                      );
         #endif
 
@@ -288,7 +262,6 @@ class Drv7Seg2x595Class {
         int32_t _status  = DRV7SEG2X595_STATUS_INITIAL;
         int32_t _variant = DRV7SEG2X595_VARIANT_INITIAL;
 
-        ByteOrder     _byte_order;
         PosSwitchType _pos_switch_type;
 
         // Used in all variants.
@@ -304,12 +277,12 @@ class Drv7Seg2x595Class {
         uint32_t _sck_pin;
         #endif
 
-        // pos_byte bits that may correspond to the actual display character positions (digits).
-        PosBit _pos_bits[DRV7SEG2X595_POS_MAX] = {PosBit::PosBitInitial,
-                                                  PosBit::PosBitInitial,
-                                                  PosBit::PosBitInitial,
-                                                  PosBit::PosBitInitial
-                                                 };
+        // Pins that may correspond to the actual display character positions (digits).
+        int32_t _pos_pins[DRV7SEG2X595_POS_MAX] = {DRV7SEG2X595_POS_PIN_INITIAL,
+                                                   DRV7SEG2X595_POS_PIN_INITIAL,
+                                                   DRV7SEG2X595_POS_PIN_INITIAL,
+                                                   DRV7SEG2X595_POS_PIN_INITIAL
+                                                  };
 
         // Glyphs assigned to be output next.
         uint8_t _pos_glyphs[DRV7SEG2X595_POS_MAX] = {0};
@@ -330,17 +303,16 @@ class Drv7Seg2x595Class {
          * valid), a negative integer otherwise (see the preprocessor macros list for possible values).
          */
         int32_t begin_helper(int32_t config_variant,
-                             ByteOrder byte_order,
                              PosSwitchType pos_switch_type,
                              uint32_t latch_pin,
 
-                             PosBit pos_1_bit,
+                             int32_t pos_1_pin,
                              /* Following parameters are assigned with values even if those were omitted
                               * in a begin_*() method call (in this case default values are assigned).
                               */
-                             PosBit pos_2_bit,
-                             PosBit pos_3_bit,
-                             PosBit pos_4_bit
+                             int32_t pos_2_pin,
+                             int32_t pos_3_pin,
+                             int32_t pos_4_pin
                             );
 
         /* Send a single byte to a shift register.
@@ -372,26 +344,13 @@ class Drv7Seg2x595Class {
 };
 
 // Class-related aliases.
-constexpr Drv7Seg2x595Class::ByteOrder Drv7SegPosByteFirst = Drv7Seg2x595Class::ByteOrder::PosByteFirst;
-constexpr Drv7Seg2x595Class::ByteOrder Drv7SegSegByteFirst = Drv7Seg2x595Class::ByteOrder::SegByteFirst;
+constexpr Drv7SegQ595Class::PosSwitchType Drv7SegActiveLow  = Drv7SegQ595Class::PosSwitchType::ActiveLow;
+constexpr Drv7SegQ595Class::PosSwitchType Drv7SegActiveHigh = Drv7SegQ595Class::PosSwitchType::ActiveHigh;
 
-constexpr Drv7Seg2x595Class::PosSwitchType Drv7SegActiveLow  = Drv7Seg2x595Class::PosSwitchType::ActiveLow;
-constexpr Drv7Seg2x595Class::PosSwitchType Drv7SegActiveHigh = Drv7Seg2x595Class::PosSwitchType::ActiveHigh;
-
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBitInitial = Drv7Seg2x595Class::PosBit::PosBitInitial;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit0       = Drv7Seg2x595Class::PosBit::PosBit0;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit1       = Drv7Seg2x595Class::PosBit::PosBit1;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit2       = Drv7Seg2x595Class::PosBit::PosBit2;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit3       = Drv7Seg2x595Class::PosBit::PosBit3;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit4       = Drv7Seg2x595Class::PosBit::PosBit4;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit5       = Drv7Seg2x595Class::PosBit::PosBit5;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit6       = Drv7Seg2x595Class::PosBit::PosBit6;
-constexpr Drv7Seg2x595Class::PosBit Drv7SegPosBit7       = Drv7Seg2x595Class::PosBit::PosBit7;
-
-constexpr Drv7Seg2x595Class::Pos Drv7SegPos1 = Drv7Seg2x595Class::Pos::Pos1;
-constexpr Drv7Seg2x595Class::Pos Drv7SegPos2 = Drv7Seg2x595Class::Pos::Pos2;
-constexpr Drv7Seg2x595Class::Pos Drv7SegPos3 = Drv7Seg2x595Class::Pos::Pos3;
-constexpr Drv7Seg2x595Class::Pos Drv7SegPos4 = Drv7Seg2x595Class::Pos::Pos4;
+constexpr Drv7SegQ595Class::Pos Drv7SegPos1 = Drv7SegQ595Class::Pos::Pos1;
+constexpr Drv7SegQ595Class::Pos Drv7SegPos2 = Drv7SegQ595Class::Pos::Pos2;
+constexpr Drv7SegQ595Class::Pos Drv7SegPos3 = Drv7SegQ595Class::Pos::Pos3;
+constexpr Drv7SegQ595Class::Pos Drv7SegPos4 = Drv7SegQ595Class::Pos::Pos4;
 
 
 /*************** GLOBAL VARIABLES ***************/
@@ -399,7 +358,7 @@ constexpr Drv7Seg2x595Class::Pos Drv7SegPos4 = Drv7Seg2x595Class::Pos::Pos4;
 /* An Arduino-style singleton object.
  * More instances of the same class can be created if necessary.
  */
-extern Drv7Seg2x595Class Drv7Seg;
+extern Drv7SegQ595Class Drv7Seg;
 
 
 #endif  // Include guards.
